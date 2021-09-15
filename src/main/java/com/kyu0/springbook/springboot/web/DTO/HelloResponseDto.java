159692880d0c5c83807fdc9c4eb5587e1c674811
Kyu0 <efftar@naver.com>
2021-09-15 14:55:23 +0900

    Assert 이용 테스트

---
+++ src/main/java/com/kyu0/springbook/springboot/web/DTO/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.kyu0.springbook.springboot.web.DTO;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+        private final String name;
+        private final int amount;
+}
